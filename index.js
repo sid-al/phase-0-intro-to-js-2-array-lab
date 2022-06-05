fd9e3bdab58c7b8e88ea66c0b947c0d084153e37
sid-al <sendplusx@gmail.com>
2022-06-05 11:49:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+cats.length();
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+    return cats;
+
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+    return cats
+}
+
+function appendCat(name) {
+    const copyCats= [...cats, 'Broom'];
+    return copyCats;
+
+}
+
+function prependCat(name) {
+    const copyCats= ['Arnold', ...cats ];
+    return copyCats;
+
+}
+
+
+function removeLastCat (name) {
+    const copyCats = cats.slice(0,-1);
+    return copyCats;
+}
+
+
+function removeFirstCat (name) {
+    const copyCats = cats.slice(1);
+    return copyCats;
+}
